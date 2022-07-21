90453fe5bbfcea8539f021cd61c5ac9af0b0bc87
supriyaDwarampudi <108483000+supriyaDwarampudi@users.noreply.github.com>
2022-07-21 09:16:08 +0530

    Adding another file

---
+++ ArithemeticOperator.java
@@ -0,0 +1,12 @@
+public class ArithemeticOperator
+{
+public static void main(String[] args)
+{
+int a=10,b=3;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+}
+}
